7e81c59a7793b5da40644e0fed63136260ee7f95
jaydip6867 <cdmi.design3@gmail.com>
2026-06-06 18:34:31 +0530

    cors error solve

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ const cors = require("cors");
 const connectDB = require("./config/db");
 
 const app = express();
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://telecaller-six.vercel.app"
+  );
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.use(cors({
-  origin: "https://telecaller-six.vercel.app/", // React frontend
+  origin: "https://telecaller-six.vercel.app",
   credentials: true
 }));
 
